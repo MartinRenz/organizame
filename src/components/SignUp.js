1773754ee5dc8b7005bf2df50e881263a354e7f1
MartinRenz <martinrenz@outlook.com>
2025-01-03 19:11:13 -0300

    Adiciona validações na tela de cadastro.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,10 +6,39 @@ const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+  const [helperText, setHelperText] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Adicionar lógica do cadastro.
+
+    // Validação de nome de usuário.
+    const usernameIsValid = username.length >= 5;
+    setUsernameError(!usernameIsValid);
+
+    // Validação do email.
+    const emailIsValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    setEmailError(!emailIsValid);
+
+    // Validação da senha.
+    const passwordIsValid = password.length >= 5;
+    setPasswordError(password.length < 5);
+
+    // Validação da confirmação da senha.
+    const confirmPasswordIsValid = password === confirmPassword;
+    setConfirmPasswordError(!confirmPasswordIsValid);
+
+    if (!usernameIsValid || !emailIsValid || !passwordIsValid || !confirmPasswordIsValid) {
+      setHelperText('Please fill in all fields correctly.');
+      return;
+    }
+
+    setHelperText('');
+
+    // Lógica do cadastro
   };
 
   return (
@@ -63,6 +92,8 @@ const SignUp = () => {
               margin="normal"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              error={usernameError}
+              helperText={usernameError ? 'Username must be at least 5 characters.' : ''}
               sx={{
                 input: {
                   color: 'white',
@@ -80,6 +111,8 @@ const SignUp = () => {
               margin="normal"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              error={emailError}
+              helperText={emailError ? 'Email is in invalid format.' : ''}
               sx={{
                 input: {
                   color: 'white',
@@ -97,6 +130,8 @@ const SignUp = () => {
               margin="normal"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              error={passwordError}
+              helperText={passwordError ? 'Password must be at least 5 characters long.' : ''}
               sx={{
                 input: {
                   color: 'white',
@@ -114,6 +149,8 @@ const SignUp = () => {
               margin="normal"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
+              error={confirmPasswordError}
+              helperText={confirmPasswordError ? 'Passwords must match.' : ''}
               sx={{
                 input: {
                   color: 'white',
@@ -140,6 +177,11 @@ const SignUp = () => {
               Sign Up
             </Button>
           </form>
+          {helperText && (
+            <Box sx={{ color: 'red', marginTop: 2, fontWeight: 'normal' }}>
+              {helperText}
+            </Box>
+          )}
         </Box>
 
         <Box
@@ -158,7 +200,7 @@ const SignUp = () => {
           <Typography sx={{ color: 'white' }}>
             Already have an account?{' '}
             <a href="/login" style={{ color: '#5494bc', textDecoration: 'none' }}>
-              Back to login.
+              Log in here.
             </a>
           </Typography>
         </Box>
